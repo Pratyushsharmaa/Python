a2397b0f56c916f4deccdb91d2e41b5bbcbb0e49
Pratyush Sharma <37974007+Pratyushsharmaa@users.noreply.github.com>
2024-02-18 22:56:43 +0530

    Update GuessTheWordGame.py

--- Python/GuessTheWordGame.py
+++ Python/GuessTheWordGame.py
@@ -1,17 +1,25 @@
-word_list = ["aardvark", "baboon", "camel"]
+#Hangman is a guessing game for two or more players. One player thinks of a word, phrase, or sentence and the other(s) tries to guess it by suggesting letters or numbers within a certain number of guesses.
 
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
-import random
+#List of words
+word_list = ["aardvark", "baboon", "camel"] 
+
+
+import random #choose a random word from the list
 chosen_word = random.choice(word_list)
 
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-
-guess = input("Guess a letter: ").lower()
-
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-
+print(f"Hello, the solution is {chosen_word}")
+display = [] #display blank char for each letter in the word
 for letter in chosen_word:
-  if letter == guess:
-    print("Right")
-  else:
-    print("Wrong")
+  display += "_"
+print(display)
+while "_" in display: #traverse until all letters are guessed
+  
+  guess = input("Guess a letter: ").lower()
+  
+  
+  for position in range(len(chosen_word)):
+    letter = chosen_word[position]
+    if letter == guess:
+      display[position] = letter
+  
+  print (display)
